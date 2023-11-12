56a2640e6aff02a59e094be1f40d62cfece8b12b
xChewy <eagis77@hotmail.com>
2023-11-11 20:35:58 -0500

    Updated for 1.20.2

--- src/main/java/me/xchewy/simplehomes/SimpleHomes.java
+++ src/main/java/me/xchewy/simplehomes/SimpleHomes.java
@@ -1,16 +1,43 @@
 package me.xchewy.simplehomes;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import static org.bukkit.Bukkit.getServer;
 
 public final class SimpleHomes extends JavaPlugin {
     @Override
     public void onEnable() {
-        Commands commands = new Commands();
-
-        getCommand("Home").setExecutor(commands);
-        getCommand("Spawn").setExecutor(commands);
-
         getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "[Simple Homes - Ver (" + getDescription().getVersion() + ")]" + ChatColor.GREEN + " is running!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!((sender) instanceof Player)) {return true;}
+        Player player = (Player) sender;
+
+        if (command.getName().equalsIgnoreCase("Home")) {
+            if (player.getBedSpawnLocation() == null) {
+                player.sendMessage(ChatColor.GOLD + "[Simple Homes]" + ChatColor.RED + " You have no bed placed!");
+            } else {
+                player.teleport(player.getBedSpawnLocation());
+                player.playSound(player.getLocation(), Sound.ITEM_CHORUS_FRUIT_TELEPORT, 1, 0.5f);
+                player.sendMessage(ChatColor.GOLD + "[Simple Homes]" + ChatColor.GREEN + " Teleporting to home!");
+            }
+        }
+
+        if (command.getName().equalsIgnoreCase("Spawn")) {
+            player.teleport(getServer().getWorld(player.getWorld().getName()).getSpawnLocation());
+            player.playSound(player.getLocation(), Sound.ITEM_CHORUS_FRUIT_TELEPORT, 1, 0.5f);
+            player.sendMessage(ChatColor.GOLD + "[Simple Homes]" + ChatColor.GREEN + " Teleporting to spawn!");
+        }
+
+        return true;
     }
 }
